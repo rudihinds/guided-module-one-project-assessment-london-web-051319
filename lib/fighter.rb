a2bb3ebf07bdf7075ddc541fdc1da86d9e6ae391
Rudi Hinds <rudihinds@gmail.com>
2019-05-29 14:51:39 +0100

    final version not refactored

---
+++ lib/fighter.rb
@@ -0,0 +1,44 @@
+class Fighter < ActiveRecord::Base
+
+  has_many :sessions
+  has_many :coaches, through: :sessions
+
+  def self.new_fighter(name)
+    fighter = self.create(name: name)
+    fighter
+  end
+
+  def self.create_new_session(coach_name, time, fighter)
+    coach = Coach.find_by(name: coach_name)
+    Session.create(coach_id: coach.id, fighter_id: fighter.id, datetime: time)
+  end
+  # rudi
+  # "Ricardo Liborio"
+
+  def my_sessions_with(name)
+    coach = Coach.find_by(name: name)
+    coach_id = coach.id
+    # how can I include a name as argument and
+    sessions = self.sessions.where(coach_id: coach_id)
+    sessions
+  end
+
+  # def update_session_time()
+
+
+
+#   belongs_to :coach
+#
+# def initialize(name: name)
+#   @name = name
+# end
+#
+#
+#
+#
+# def trainer
+#     # Trainer.all.find{ |trainer| trainer.id == self.trainer_id }
+#     # OR use AR .find
+#     Trainer.find(self.trainer_id)
+#   end
+end
